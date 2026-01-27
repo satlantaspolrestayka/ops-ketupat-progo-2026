40e113af63d20ec192954e96a90da2dbd58a552b
satlantasyka <unitkamselpolrestayka@gmail.com>
2026-01-27 16:09:58 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Service Worker untuk caching
+const CACHE_NAME = 'parkir-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
